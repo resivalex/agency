81c0630ee41dcb802e0b5c2e884ee052d52d25a8
Иван Решетников <resivalex@gmail.com>
2016-09-25 19:18:08 +0300

    Add resume CRUD actions

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,5 +1,58 @@
 class ResumesController < ApplicationController
+  before_action :find_resume, only: [:show, :edit, :update, :destroy]
+
   def index
     @resumes = Resume.all
+  end
+
+  def show
+  end
+
+  def new
+    @resume = Resume.new
+  end
+
+  def create
+    @resume = Resume.new(resume_params)
+    if @resume.save
+      redirect_to edit_resume_path(@resume), notice: 'Created'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @resume.update(resume_params)
+      redirect_to edit_resume_path(@resume), notice: 'Updated'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @resume.destroy
+      redirect_to resumes_path, notice: 'Deleted'
+    else
+      redirect_to resume_path, alert: 'Can\'t delete'
+    end
+  end
+
+  private
+
+  def find_resume
+    @resume = Resume.find(params[:id])
+  end
+
+  def resume_params
+    params.required(:resume).permit(:first_name,
+                                    :last_name,
+                                    :middle_name,
+                                    :phone,
+                                    :email,
+                                    :desired_salary,
+                                    :is_active)
   end
 end
--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,6 +1,8 @@
 class Resume < ActiveRecord::Base
   include Skillable
 
+  validates :first_name, :last_name, :phone, :email, :desired_salary, presence: true
+
   def full_name
     "#{last_name} #{first_name} #{middle_name}".squish
   end
